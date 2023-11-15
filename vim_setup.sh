decfa6de8ca7271bbbf4be881fa848598e9b7706
soohyuncha <49219495+soohyuncha@users.noreply.github.com>
2023-11-15 13:55:33 +0900

    Update vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# 1) Copy vimrc
+# 1) Copy vimrc (If you want to use my vimrc, then uncomment following single line)
 # cp vimrc ~/.vimrc
 
-# 2) Open vim and PluginInstall
-#   Then restart vim
+# 2) Open vim and type 'PluginInstall' in vim command
+
+# 3) Restart vim
